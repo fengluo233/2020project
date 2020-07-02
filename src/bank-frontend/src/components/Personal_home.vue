0d0ff50967078ff369bd838caf6c0bc2fd6e3b10
unknown <2389236418@qq.com>
2020-07-02 22:46:04 +0800

    加点新界面

--- src/bank-frontend/src/components/Personal_home.vue
+++ src/bank-frontend/src/components/Personal_home.vue
@@ -1,5 +1,6 @@
 <template>
-<el-container style="height: 500px; border: 1px solid #eee">
+<div>
+<el-container >
   <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
     <el-menu :default-openeds="['1', '3']">
       <el-submenu index="1">
@@ -71,6 +72,7 @@
     </el-main>
   </el-container>
 </el-container>
+</div>
 </template>
  
 <script>
