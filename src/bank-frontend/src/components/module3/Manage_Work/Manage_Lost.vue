886b8db89e6b434c0a7f6a0d80482b5cec1d1541
RuShMaN2019 <gongziang@gongziangdeMacBook-Air.local>
2020-07-03 17:06:01 +0800

    update module3

--- src/bank-frontend/src/components/module3/Manage_Work/Manage_Lost.vue
+++ src/bank-frontend/src/components/module3/Manage_Work/Manage_Lost.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
+
+<el-container class="com_home_container">
+        <!-- 头部区域-->
+        <el-header>
+            <div>
+                <span>操作员处理界面</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <!-- 页面主体区域-->
+        <el-container>
+          <!-- 侧边栏区域-->
+         <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  
+  <el-submenu index="2">
+    <template slot="title">用户信息</template>
+    <el-menu-item index="2-1" @click="checkinfo">查看用户信息</el-menu-item>
+    <el-menu-item index="2-2" @click="modifyinfo">修改用户信息</el-menu-item>
+  </el-submenu>
+  <el-menu-item index="1" @click="freeze">账户冻结</el-menu-item>
+  <el-menu-item index="3" @click="lost">用户挂失</el-menu-item>
+  
+</el-menu>
+
+        </el-container>
+       <div>
     <el-form ref="lostform" :model="form" :rules="rules" label-width="80px" class="lost-box">
       <h3 class="lost-title">用户挂失</h3>
       <el-form-item label="用户名" prop="name">
@@ -20,6 +45,10 @@
     </el-form>
 
   </div>
+    </el-container>
+
+
+  
 </template>
 
 <script>
@@ -54,6 +83,24 @@ import axios  from 'axios'
       }
     },
     methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      logout(){
+        this.$router.replace({name:'Manage_Login'})
+      },
+      checkinfo(){
+        this.$router.push({name:'Manage_Check'})
+      },
+      modifyinfo(){
+        this.$router.push({name:'Manage_Modify'})
+      },
+      freeze(){
+        this.$router.push({name:'Manage_Freeze'})
+      },
+      lost(){
+        this.$router.push({name:'Manage_Lost'})
+      },
       onSubmit(formName) {
         // 为表单绑定验证功能
         this.$refs[formName].validate((valid) => {
@@ -79,6 +126,37 @@ import axios  from 'axios'
 </script>
 
 <style  scoped>
+.el-menu.el-menu--horizontal {
+    border-bottom:0px !important;
+}
+.com_home_container{
+    height: 100%;
+}
+.el-header{
+    background-color: #808a87;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    font-size: 20;
+}
+
+.el-aside{
+    background-color:cadetblue;
+}
+
+.el-aside{
+    background-color: #eaedf1;
+}
+.toggle-button{
+    background-color: #cccccc;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+}
   .lost-box {
     border: 1px solid #DCDFE6;
     width: 350px;
@@ -95,6 +173,14 @@ import axios  from 'axios'
     margin: 0 auto 40px auto;
     color: #303133;
   }
+  .el-container {
+    display: block!important;
+    flex-direction: row;
+    flex: 1;
+    flex-basis: auto;
+    box-sizing: border-box;
+    min-width: 0;
+}
 </style>
 
 
